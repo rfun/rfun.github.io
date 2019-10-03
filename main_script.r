f5b344233bb03e860aee1af5c7c632d20ca4a56d
Rohit Khattar <rohitkh@byu,edu>
2019-10-03 12:21:14 -0600

    Added R Code

---
+++ main_script.r
@@ -0,0 +1,17 @@
+
+# Create a basic scatterplot, shows degree of corelation between different predictor variables
+
+# Load data file about Google searches by state
+google <- read.csv("google_correlate.csv", header = T)
+names(google)
+
+# Basic Scatterplot Matrix
+pairs(~data_viz + degree + facebook + nba,
+      data = google, 
+      pch = 20,
+      main = "Simple Scatterplot Matrix")
+
+# Use "Pairs Plot" from "psych" package
+install.packages("psych")
+library("psych")
+pairs.panels(google[c(3, 7, 4, 5)], gap = 0)
